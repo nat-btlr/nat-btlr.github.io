eb8a406625d5c35ef370ace91790da5d0cd33360
Natalia Sugrobova <natalyasugrobova@gmail.com>
2024-07-19 16:53:23 +0200

    Desktop without js done

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
-  /**
-   * Skills animation
-   */
-  let skillsContent = document.querySelector('.technical_competences');
-  if (skillsContent) {
-    new Waypoint({
-      element: skillsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = document.querySelectorAll('.progress .progress-bar', true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
-        });
-      }
-    })
-  }
+
+
+on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
